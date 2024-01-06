7f4067d936a0600382c6911e839345bfdf3e3bcd
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2024-01-06 02:36:29 +0100

    File repository organized + server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+//const nodemailer = require("nodemailer");
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
