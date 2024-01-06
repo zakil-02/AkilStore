996c64d65655c852a1673aacd4607d4f6d34ec0a
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2024-01-06 03:46:36 +0100

    Product card component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import  PcardColored  from './components/Pcard';
+
+function App() {
+  return (
+    <div className="App">
+      <PcardColored />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Pcard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const PcardColored = () => {
+  return (
+    <div class="card">
+        <div class="image"><span class="text">This is a chair.</span></div>
+        <span class="title">Cool Chair</span>
+        <span class="price">$100</span>
+    </div>
+  );
+}
+
+export default PcardColored;
